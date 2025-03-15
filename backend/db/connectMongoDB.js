3403ce825fd14368cbb815369fb483e9fd384992
Samuel Bolibruch <samo.bolibruch@gmail.com>
2025-03-15 17:41:43 +0100

    home api route

--- backend/db/connectMongoDB.js
+++ backend/db/connectMongoDB.js
@@ -1,11 +1,14 @@
+
 import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
   try {
+    console.log("Attempting to connect to MongoDB...");
+    console.log(`Mongo URI: ${process.env.MONGO_URI}`);
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`Error connection to mongoDB: ${error.message}`);
+    console.error(`Error connecting to MongoDB: ${error.message}`);
     process.exit(1);
   }
 };
--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,10 @@ app.use(express.json({ limit: "5mb" })); // to parse req.body
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("WELCOME TO BOOK API!");
+});
+
 app.use("/api/books", bookRoutes);
 app.use("/api/readers", readerRoutes);
 app.use("/api/loans", loanRoutes);
